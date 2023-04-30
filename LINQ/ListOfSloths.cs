877f6c4446bb97039480d3769e9ab971f571a649
Adam Jankowski <ghost17814@gmail.com>
2023-04-30 16:43:27 -0400

    yeeyee

---
+++ LINQ/ListOfSloths.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    public class ListOfSloths
+    {
+        public static List<Sloths> GetSloths()
+        {
+            return new List<Sloths>
+            {
+                new Sloths {DiffSloths = "Pygmy three-toed sloth"},
+                new Sloths {DiffSloths = "Maned sloth"},
+                new Sloths {DiffSloths = "Pale-throated sloth"},
+                new Sloths {DiffSloths = "Brown-throated sloth"}, 
+                new Sloths {DiffSloths = "Linnaeus’s two-toed sloth"},
+                new Sloths {DiffSloths = "Hoffman’s two-toed sloth"}
+            };
+            //return new List<Sloths>
+            //{
+            //    new Sloths {GenusSpecies  = "Bradypus pygmaeus"},
+            //    new Sloths {GenusSpecies = "Bradypus torquatus"},
+            //    new Sloths {GenusSpecies = "Bradypus tridactylus" },
+            //    new Sloths {GenusSpecies = "Bradypus variegatus"},
+            //    new Sloths {GenusSpecies = "Choloepus didactylus"},
+            //    new Sloths {GenusSpecies = "Choloepus hoffmanni"}
+            //};
+        }
+    }
+
+}
--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace LINQ
+﻿using System.Security.Cryptography;
+using System.Threading.Channels;
+using System.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {            
+            var zachs = ListOfSloths.GetSloths();
+            var slothABCs = zachs.OrderBy(name => name.DiffSloths);
+            foreach ( var s in slothABCs)
+            {
+                Console.WriteLine(s.DiffSloths);
+            }
         }
     }
 }
